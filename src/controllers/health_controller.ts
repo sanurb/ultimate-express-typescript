a206400c30fc78c9f5bfe88b6282daacc21220dc
Santiago U <60886336+sanurb@users.noreply.github.com>
2025-04-07 09:48:11 -0500

    chore: move features to folder 'tests'

--- src/controllers/health_controller.ts
+++ src/controllers/health_controller.ts
@@ -1,6 +1,7 @@
 import type { Request, Response } from "ultimate-express";
 import { Status } from "@reflet/http";
 
-export const healthController = (_req: Request, res: Response): void => {
-	res.status(Status.Ok).send("OK");
-};
+/** Responds with a 200 OK status and 'OK' message. */
+export function getHealth(_req: Request, res: Response): void {
+    res.status(Status.Ok).send("OK");
+}
--- tests/e2e/health.spec.ts
+++ tests/e2e/health.spec.ts
@@ -1,34 +1,24 @@
-import { describeFeature, loadFeature } from "@amiceli/vitest-cucumber";
-import { expect } from "vitest";
-import request from "supertest";
-import app from "../../src/app";
-import { beforeAll, afterAll } from "vitest";
+import { describeFeature, loadFeature } from '@amiceli/vitest-cucumber'
+import { expect } from 'vitest'
+import path from 'node:path'
+import request from 'supertest'
 
-const feature = await loadFeature("features/health.feature");
-let server: ReturnType<typeof app.listen>;
-
-beforeAll(() => {
-	server = app.listen(3000);
-});
-
-afterAll(() => {
-	server.close();
-});
+const feature = await loadFeature(path.join(__dirname, '../features/health.feature'))
 
 describeFeature(feature, ({ Scenario }) => {
-	Scenario("Check if the server is alive", ({ When, Then, And }) => {
-		let response: request.Response;
+    Scenario('Check if the server is alive', ({ When, Then, And }) => {
+        let response: request.Response
 
-		When('I send a GET request to "/health"', async () => {
-			response = await request("http://localhost:3000").get("/health");
-		});
+        When('I send a GET request to "/health"', async () => {
+            response = await request('http://localhost:3000').get('/health')
+        })
 
-		Then("I should receive a 200 status code", () => {
-			expect(response.status).toBe(200);
-		});
+        Then('I should receive a 200 status code', () => {
+            expect(response.status).toBe(200)
+        })
 
-		And('the response should be "OK"', () => {
-			expect(response.text).toBe("OK");
-		});
-	});
-});
+        And('the response should be "OK"', () => {
+            expect(response.text).toBe('OK')
+        })
+    })
+})
